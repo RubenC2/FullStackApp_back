ba324690432fe48a83f32ac8f7c05041a9afc09e
RubenC2 <rubenct1993@gmail.com>
2024-11-26 17:37:26 +0100

    conex ok, index ok, midd Manage y Morgan ok

---
+++ utils/tablas.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    logged boolean,
+    rol VARCHAR(10) DEFAULT 'user' CHECK (rol IN ('admin', 'user'))
+);
+
+INSERT INTO users (nombre, apellidos, email, password, logged, rol) VALUES 
+  ('Dida',  'Castro', 'didaxample@gmail.com', '123456', 'false', 'admin'),
+  ('Rubén', 'Castro', 'rubenexample@gmail.com', '123456', 'false', 'user')

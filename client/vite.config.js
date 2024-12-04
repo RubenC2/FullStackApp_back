14b282a34cef21e6af36e8752b90ed4b1503d2f2
RubenC2 <rubenct1993@gmail.com>
2024-12-04 14:38:30 +0100

    tryDockerfile

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../dist',  // Asegúrate de que los archivos estáticos se generen en la carpeta `dist`
+    outDir: '../dist', 
   }
 })
 

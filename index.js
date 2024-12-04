11a1382b2e545c1d5acd7dd90ecbc1e7b3b8d9f1
RubenC2 <rubenct1993@gmail.com>
2024-12-04 13:07:49 +0100

    fixedErrorDeploy

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use('/api/categorias', categoriaRoutes);
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
   });
 
 app.listen(port, () => {
